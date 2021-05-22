a44346291f7b3167d9d643e9b4c36fa379daaf2f
Naira777 <60170665+Naira777@users.noreply.github.com>
2021-05-23 00:25:45 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import DisplayWeather from "./components/DisplayWeather/DisplayWeather";
+import Form from "./components/Form/Form";
+import * as Styled from "./styled"
+
+function App() {
+  return (
+  <Styled.AppContent>
+    <>
+      <Form  />
+      <DisplayWeather />  
+    
+   </>
+    </Styled.AppContent>
+  );
+}
+
+export default App;
