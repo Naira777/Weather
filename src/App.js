8652a2ca3209626372f4f4641e4bee1c3dd758f2
Naira777 <60170665+Naira777@users.noreply.github.com>
2022-05-03 01:51:59 +0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,102 @@
-import DisplayWeather from "./components/DisplayWeather/DisplayWeather";
-import Form from "./components/Form/Form";
-import * as Styled from "./styled"
+import React, {useEffect, useRef, useState, useLayoutEffect} from 'react'
+import './App.css';
+import video from '../src/Video/video.mp4'
+import useOnScreen from './Visible';
+
+
 
 function App() {
-  return (
-  <Styled.AppContent>
-    
-      <Form  />
-      <DisplayWeather />  
-    
-   
-    </Styled.AppContent>
-    
-  );
+
+const [anim, setAnim] = useState(false)
+const [anim1, setAnim1] = useState(false)
+const ref= useRef()
+
+
+const isVisible = useOnScreen(ref);
+
+
+
+const handleScroll =() => {
+
+setAnim(true)
+
+
+}
+function openFullscreen(elem) {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
 }
 
+
+const handleScroll1 =() => {
+
+  setAnim(true)
+ openFullscreen(document.getElementById("video"))
+}
+
+
+useEffect(()=> {
+
+window.addEventListener('scroll',  handleScroll1 )
+
+
+
+
+return () =>{
+
+
+window.removeEventListener('scroll', handleScroll1)
+}
+
+},[anim])
+
+
+
+
+  return (   
+  
+  
+  <div className="App" >
+      
+     
+  { !anim && <video className="v"    muted
+  autoPlay={"autoplay"}
+  preLoad="auto"
+  loop> 
+  <source src={video} type="video/mp4">
+
+   </source>
+
+   </video>}
+
+  {isVisible && <video className="" ref={ref} muted
+  autoPlay={"autoplay"}
+  preLoad="auto"
+  loop
+  src={video} type="video/mp4">
+ 
+   </video>}
+
+  {anim && <video className="vanima"  muted
+  autoPlay={"autoplay"}
+  preLoad="auto"
+  loop
+  src={video} type="video/mp4">
+ 
+   </video>}
+   
+
+ 
+      
+    </div>
+  );
+}
+   
 export default App;
+
+
